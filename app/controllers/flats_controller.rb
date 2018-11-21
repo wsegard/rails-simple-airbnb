7b1177f819145e62da4dbc58bd8729628e1f0a97
william segard <william.segard@gmail.com>
2018-11-21 17:37:02 +0100

    airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,15 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark & Small Flat London',
+  address: '10 Liverpool Street W9 1DT',
+  description: 'Awful place',
+  price_per_night: 150,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Wonderful penthouse London',
+  address: '105 Curtain Road SE1 3FB',
+  description: '2 rooms, huge living room with rooftop terrace',
+  price_per_night: 300,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Castle in London',
+  address: '3 Leathermarket Street',
+  description: '15 rooms, 3 living rooms',
+  price_per_night: 15000,
+  number_of_guests: 25
+)
